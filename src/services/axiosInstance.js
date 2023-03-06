84ca7bf6d35a9042d0c7eac5a72d21af88463fad
GhifarM <emptylost445@gmail.com>
2023-03-06 14:07:34 +0700

    Update URL BASE DEV

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosinstance = axios.create({
-    baseURL: "http://172.16.5.139:3001/",
+    baseURL: "", //URL BASE
     headers: {
         "Content-Type": "application/json",
         Authorization: "Bearer " + window.localStorage.getItem('token'),
